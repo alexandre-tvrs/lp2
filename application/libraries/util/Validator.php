be0e8d1b656cf45c6c21083c71aaf2e73633e85d
Alexandre Tavares <alexandre.j.tavares.jr@gmail.com>
2022-05-02 21:09:31 -0300

    Second commit

---
+++ application/libraries/util/Validator.php
@@ -0,0 +1,17 @@
+<?php
+class Validator {
+
+  public static function validateUser() {
+    $this->form_validation->set_rules('nome', 'Nome da pessoa', 'required | min_lenght[2] | max_lenght[19]');
+    $this->form_validation->set_rules('snome', 'Sobrenome da pessoa', 'required | min_lenght[2] | max_lenght[256]');
+    $this->form_validation->set_rules('email', 'Endereço eletrônico', 'required | valid_email');
+    $this->form_validation->set_rules('senha', 'Senha', 'required | min_lenght[8] | max_lenght[32]');
+    $this->form_validation->set_rules('conf_senha', 'Confirmação da Senha', 'required | match[senha]');
+    $this->form_validation->set_rules('telefone', 'Telefone', 'min_lenght[10] | max_lenght[14]');
+
+    return $this->form_validation->run();
+
+  }
+
+}
+?>
